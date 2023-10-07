cd9e05fbcd2ff13ff5c292993bb6c17b0f15fed2
MITHUN MOHANDAS <mithunsruthi@gmail.com>
2023-10-07 13:17:21 +0530

    toggle button implementation

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import Header from './components/Header';
 import ClassDemo from './components/ClassDemo'
 import Counter from './components/Counter';
 import Employees from './Employees'
+import Counter2 from './components/Counter2';
+import { useState } from 'react';
 
 
 //functional component
 function App() {
   const User = "Mithun Mohandas";
   const EmpData = [
-    {name : 'Elon', age: 46 , email:"elonmusk@tesla.com"},
-    {name : 'Mark', age: 43, email:"markzuckerberg@facebook.com"},
-    {name : 'Steve', age: 88, email:"stevejobs@apple.com"},
-    {name : 'Bill', age: 74, email:"billgates@microsoft.com"},
-    {name : 'Mukesh', age: 65, email:"mukeshambani@reliance.in"},
-  ]
-  
+    { name: 'Elon', age: 46, email: "elonmusk@tesla.com" },
+    { name: 'Mark', age: 43, email: "markzuckerberg@facebook.com" },
+    { name: 'Steve', age: 88, email: "stevejobs@apple.com" },
+    { name: 'Bill', age: 74, email: "billgates@microsoft.com" },
+    { name: 'Mukesh', age: 65, email: "mukeshambani@reliance.in" },
+  ];
+
+  const [state, setState] = useState(false)
+
 
   return (
     <div>
@@ -32,14 +36,20 @@ function App() {
       <h1 className='text-center my-3'>Employee Data</h1>
 
       {/* javascript map method */}
-      { EmpData.map((obj,index)=>{
-        return(
+      {EmpData.map((obj, index) => {
+        return (
           <Employees key={index} {...obj} /> // using spread operator 
         )
 
-      }) }
+      })}
 
+      <hr className='mt-5' />
+      
+      <div className="text-center">
+      <button className='btn btn-danger m-3' onClick={()=> {setState(!state)}}>Show/Hide</button>
+      </div>
 
+      {state && <Counter2 /> }
       <hr className='mt-5' />
       <ClassDemo message={User} />
     </div>
---
+++ src/components/Counter2.js
@@ -0,0 +1,21 @@
+import React,{useState, useEffect} from 'react'
+
+function Counter2() {
+    const [count,setCount] = useState(0)
+    useEffect(()=>{
+        console.log("Mounted");
+        console.log("update"+count)
+
+        return (()=>{
+            console.log("Unmounted"+count);
+        })
+    },[count]) 
+  return (
+    <div className='App'>
+        <h1> Hidden conunter 1 value : {count} </h1>
+        <button onClick={()=>{setCount(count+1)}} className='btn btn-primary'>ADD</button>
+    </div>
+  )
+}
+
+export default Counter2
